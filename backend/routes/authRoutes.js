6a2bb804e9cad08c69db8c5c315e7e39f25a406e
Anatoly <tolyamelnik020406@gmail.com>
2026-04-24 00:11:37 +0800

    create auth routes.

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { register, login, getMe } from "../conrollers/authController.js";
+import { protect } from "../middlewares/authMiddleware.js";
+import upload from "../middlewares/uploadMiddleware.js";
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/me", protect, getMe);
+
+router.post("/upload-image", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded" });
+  }
+  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+  res.status(200).json({ imageUrl });
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 import path from "node:path";
 import connectDB from "./config/db.js";
-// import { authRoutes } from "./routes/authRoutes";
+import { authRoutes } from "./routes/authRoutes.js";
 
 const app = express();
 
@@ -23,7 +23,7 @@ connectDB();
 app.use(express.json());
 
 // Routes
-// app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
 // Server upload folder
 app.use(
